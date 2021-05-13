3f19bfc5dc4b3ed4b1a498041712db20c6de0c96
andrushens <thelasmo3228@gmail.com>
2021-05-13 19:38:22 +0300

    gitignore + clean

--- lab1/script.py
+++ lab1/script.py
@@ -1,5 +1,4 @@
 def solve():
-    """Дана строка S и Q запросов вида L R - промежуток, на котором нужно инвертировать регистр символов. Требутеся найти строку S после выполнения всех запросов"""
     S = input()
     Q = int(input())
     ans = ""
@@ -10,8 +9,7 @@ def solve():
             changes[j] ^= 1
     for i in range(len(S)):
         ans += S[i].swapcase() if changes[i] else S[i]
-    print(ans)
+    return ans
 
 
-print(solve.__doc__)
-solve()
+print(solve())
