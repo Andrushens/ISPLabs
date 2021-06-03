4fee7b24ef69718865489f4cfe7a0e8d2b27ea15
andrushens <thelasmo3228@gmail.com>
2021-06-04 01:20:38 +0300

    added login+register

--- lab3/main/urls.py
+++ lab3/main/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
+    path('', views.homePage, name='home'),
+    path('login', views.loginPage, name='login'),
+    path('signup', views.signupPage, name='sign-up'),
+    path('logout', views.logoutPage, name='logout'),
 ]
--- lab3/main/views.py
+++ lab3/main/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from main.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import SignupForm
+
+
+@login_required(login_url='login')
+def homePage(request):
+    return render(request, 'main/index.html')
+
+
+def signupPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    form = SignupForm()
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account successfully created')
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'main/sign-up.html', context)
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+        
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        
+        messages.info(request, 'Incorrect login or password')
+
+    context = {}
+    return render(request, 'main/login.html', context)
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
